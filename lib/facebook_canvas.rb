f431d109b80f8bcdf4b04bd1ab0a0e1e4a73107b
Peter Suschlik <ps@neopoly.de>
2015-10-30 15:00:48 +0100

    This gem is Rails-agnostic!

--- lib/facebook_canvas.rb
+++ lib/facebook_canvas.rb
@@ -1,4 +1,4 @@
-require "facebook_canvas/engine"
+require "facebook_canvas/engine" if defined? Rails
 require "facebook_canvas/middleware"
 require "facebook_canvas/helpers"
 require "facebook_canvas/signed_request"
