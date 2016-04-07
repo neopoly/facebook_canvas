0e08705f09b58ae59c9f086fdb62db11ca65ea61
René Föhring <rf@bamaru.de>
2016-04-07 08:49:03 +0200

    Add constants for default values in Engine

--- lib/facebook_canvas/engine.rb
+++ lib/facebook_canvas/engine.rb
@@ -1,8 +1,11 @@
 module FacebookCanvas
   class Engine < ::Rails::Engine
+    DEFAULT_SERVER_REGEX = /.*/
+    DEFAULT_CUSTOM_FILTER = proc { |_env| true }
+
     initializer "FacebookCanvas.middleware" do |app|
-      server_name = FacebookCanvas.server_name || /.*/
-      custom_filter = FacebookCanvas.custom_filter || proc { |_env| true }
+      server_name = FacebookCanvas.server_name || DEFAULT_SERVER_REGEX
+      custom_filter = FacebookCanvas.custom_filter || DEFAULT_CUSTOM_FILTER
       app.config.middleware.use FacebookCanvas::Middleware, server_name, custom_filter
 
       ApplicationController.prepend FacebookCanvas::Helpers
